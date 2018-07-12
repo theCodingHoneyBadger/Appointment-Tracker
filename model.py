9853cc6f8701ac4f9f5192a0b1f04ad37aba66ba
Jason Morrissette <mythicalbromace@gmail.com>
2018-07-12 10:00:26 -0700

    Feat: Created Data Model
    Created Data Model for Appointment-Tracker

--- model.py
+++ model.py
@@ -1,1 +1,21 @@
 ## Database for The Appointment Tracker Applciation ##
+import os
+import sys
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+## Customer Table ##
+class Customer(Base):
+    __tablename__='customers'
+    id = Column(Integer, primary_key=True)               # Customer ID
+    first_name = Column(String(250), nullable=False)     # Customer First Name
+    last_name = Column(String(250), nullable=False)      # Customer Last Name
+    time = Column(String(250), nullable=False)           # Customer Appointment Time
+
+
+engine = create_engine('sqlite:///sqlalchemy_customer.db')
+Base.metadata.create_all(engine)
